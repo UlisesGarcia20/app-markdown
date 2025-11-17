0e0dd66044d360649d7afbfa2ebabed0c51e6368
Yexzari Panes <yexzari.panes@innadvisory.com>
2025-11-17 14:15:36 -0800

    New changes in app.txs

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,6 @@ function App() {
         showSuccess,
         downloadedFileName,
         handleFilesSelected,
-        handleRemoveFile,
-        handleClearAll,
         handleNewConversion,
         handleUpload,
     } = useFileUpload();
